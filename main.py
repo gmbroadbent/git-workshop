c90c74d33c6eaf81c7127353640dbe29f89d1e7d
mark <mark@gmail.com>
2019-10-15 20:13:58 +0100

    Horses

--- main.py
+++ main.py
@@ -2,4 +2,5 @@
 print("There was once a man named Tito.")
 ships = input("Tell me how many ships he had.")
 print("He had... " , ships , " warships.")
+print("And {} horses".format(ships+ships))
 #Do Nothing
